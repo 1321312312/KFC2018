63641eceb0f88f4050311accc110cf841b88b6a8
too405 <38613331+toomuchm@users.noreply.github.com>
2018-04-27 18:46:52 +0800

    Update update1.sh

--- update1.sh
+++ update1.sh
@@ -3,7 +3,8 @@ echo "欢迎使用网页雷达一键脚本"
 echo "即将搭建4.27版本"
 echo "请输入你的内网ip" 
 read -p "内网ip： " ip
-rm -rf KFC2018
+cd libpcap-1.8.1
+rm -rf lKFC2018/
 cp /root/KFC2018/restart.sh /root/restart.sh
 chmod +x restart.sh
 git clone https://github.com/1321312312/KFC2018.git
